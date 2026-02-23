3b091c3790cb277276330f34c51b50643fecec42
pramith111 <lpramith111@gmail.com>
2026-02-23 15:48:17 +0530

    Version 2 - Added validation

--- src/main/java/com/pl3001/guessingapp/GuessingApp.java
+++ src/main/java/com/pl3001/guessingapp/GuessingApp.java
@@ -1,12 +1,35 @@
-//Author Developer
-//Version 1
+/*
+Author-Developer
+Version-2
+*/
 package com.pl3001.guessingapp;
-public class GuessingApp
-{
-	public static void main(String [] args)
-	{
-		System.out.println("Welcome to the Guessing App");
-		GameConfig gameConfig=new GameConfig();
-		gameConfig.showRules();
+import java.util.Scanner;
+public class GuessingApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Guessing App");
+
+        GameConfig config = new GameConfig();
+        config.showRules();
+
+        Scanner scanner = new Scanner(System.in);
+        int attempts = 0;
+
+ 
+       while (attempts < config.getMaxAttempts()) {
+
+		System.out.print("Enter your guess: ");
+		int guess = scanner.nextInt();
+		attempts++;
+
+		String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
+		System.out.println(result);
+
+		if ("CORRECT".equals(result)) {
+			break;
+		}
+		}
+    
 	}
 }
