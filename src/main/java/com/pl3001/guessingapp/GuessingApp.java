e9e058fd825d97824f02b29b14b63a945429f9e3
pramith111 <lpramith111@gmail.com>
2026-02-23 16:11:29 +0530

    Version 4 - Added hints and validation improvements

--- src/main/java/com/pl3001/guessingapp/GuessingApp.java
+++ src/main/java/com/pl3001/guessingapp/GuessingApp.java
@@ -1,35 +1,45 @@
-/*
-Author-Developer
-Version-2
-*/
+//Author Developer
+//Version 4
 package com.pl3001.guessingapp;
+
 import java.util.Scanner;
+
 public class GuessingApp {
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to the Guessing App");
+    public static void main(String[] args) throws InvalidInputException {
 
         GameConfig config = new GameConfig();
-        config.showRules();
-
         Scanner scanner = new Scanner(System.in);
+
         int attempts = 0;
+        int hintsUsed = 0;
 
- 
-       while (attempts < config.getMaxAttempts()) {
+        while (attempts < config.getMaxAttempts()) {
 
-		System.out.print("Enter your guess: ");
-		int guess = scanner.nextInt();
-		attempts++;
+            System.out.print("Enter your guess: ");
+            int guess = ValidationService.validateInput(scanner.nextLine());
+            attempts++;
 
-		String result = GuessValidator.validateGuess(guess, config.getTargetNumber());
-		System.out.println(result);
+            String result = GuessValidator.validateGuess(
+                    guess, config.getTargetNumber());
 
-		if ("CORRECT".equals(result)) {
-			break;
-		}
-		}
-    
-	}
+            if (!"CORRECT".equals(result)
+                    && hintsUsed < config.getMaxHints()) {
+
+                hintsUsed++;
+                System.out.println(
+                        HintService.generateHint(
+                                config.getTargetNumber(),
+                                hintsUsed));
+            }
+
+            System.out.println(result);
+
+            if ("CORRECT".equals(result)) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
 }
